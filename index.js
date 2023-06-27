535f4b54bce668aec15d15edf45cd56126f2c3a0
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-06-27 20:19:40 +0100

    Update index.js
    Made it so text on the consent form changes after 1.5 seconds of being submitted
    Added a third modal message using innerHTML.
    Added a const to grab the full name that is typed into the consent form and display it on the page on the third modal

--- index.js
+++ index.js
@@ -14,12 +14,34 @@ modalCloseBtn.addEventListener('click', function () {
 
 consentForm.addEventListener('submit', function (e) {
     e.preventDefault()
+
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+
     modalText.innerHTML =
         `<div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
-    <p id="uploadText">
+    <p id="upload-text">
         Uploading your data to the dark web...
     </p>
 </div> `
+
+    setTimeout(function () {
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+
+    setTimeout(function () {
+        document.getElementById('modal-inner').innerHTML = `
+    <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+<p>We just sold the rights to your eternal soul.</p>
+<div class="idiot-gif">
+    <img src="images/pirate.gif">
+</div>
+`
+    }, 1500)
 })
+
+
+
 
